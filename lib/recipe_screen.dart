26b93419d7121bbd39e278f859a6d7df09f817cc
Kishan <darjikishan2002@gmail.com>
2023-03-02 20:30:16 +0530

    final1

--- lib/recipe_screen.dart
+++ lib/recipe_screen.dart
@@ -1,26 +1,26 @@
-// import 'package:flutter/material.dart';
-// import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
-// class RecipeScreen extends StatefulWidget {
-//   final String url;
+class RecipeScreen extends StatefulWidget {
+  final String url;
 
-//   RecipeScreen({super.key, required this.url});
+  RecipeScreen({super.key, required this.url});
 
-//   WebViewController controller = WebViewController()
-//     ..setJavaScriptMode(JavaScriptMode.unrestricted)
-//     ..loadRequest(Uri.parse());
+  WebViewController controller = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..loadRequest(Uri.parse('https://flutter.dev'));
 
-//   @override
-//   State<RecipeScreen> createState() => _RecipeScreenState();
-// }
+  @override
+  State<RecipeScreen> createState() => _RecipeScreenState();
+}
 
-// class _RecipeScreenState extends State<RecipeScreen> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text("Recipe"),
-//       ),
-//     );
-//   }
-// }
+class _RecipeScreenState extends State<RecipeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Recipe"),
+      ),
+    );
+  }
+}
